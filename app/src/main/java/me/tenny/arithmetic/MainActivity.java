c27bde5967aa8254cc7c20ab753875f3bf883754
tennywu <tenny.wu86@gmail.com>
2018-07-10 17:47:15 +0800

    开始

--- app/src/main/java/me/tenny/arithmetic/MainActivity.java
+++ app/src/main/java/me/tenny/arithmetic/MainActivity.java
@@ -3,6 +3,10 @@ package me.tenny.arithmetic;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 剑指 offer 题解
+ * https://github.com/CyC2018/Interview-Notebook/blob/master/notes/%E5%89%91%E6%8C%87%20offer%20%E9%A2%98%E8%A7%A3.md
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
